fb9ed15df16e0588803ec023752d528fc5562b9d
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2023-05-29 23:34:56 -0300

    Configurações servidor

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+const sumbit = document.getElementById('button')
+
+sumbit.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const file = document.getElementById('file');
+
+    /*
+    const fileToRead = file.files[0];
+    const fileReader = new FileReader();
+
+    fileReader.readAsText(fileToRead)
+    fileReader.onload = ()=>{
+        const data = fileReader.result;
+        console.log(data)
+    }
+    */
+
+   
+    
+})
+
